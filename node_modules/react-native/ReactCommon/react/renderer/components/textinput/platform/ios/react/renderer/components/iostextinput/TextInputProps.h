0764c2e89b1c0cca0069caaa5f2fc9483bb75cd2
skebila <stevenkebila@gmail.com>
2025-03-02 15:33:19 -0500

    React native frontend init

--- node_modules/react-native/ReactCommon/react/renderer/components/textinput/platform/ios/react/renderer/components/iostextinput/TextInputProps.h
+++ node_modules/react-native/ReactCommon/react/renderer/components/textinput/platform/ios/react/renderer/components/iostextinput/TextInputProps.h
@@ -37,7 +37,6 @@ class TextInputProps final : public BaseTextInputProps {
   std::optional<Selection> selection{};
 
   const std::string inputAccessoryViewID{};
-  const std::string inputAccessoryViewButtonLabel{};
 
   bool onKeyPressSync{false};
   bool onChangeSync{false};
